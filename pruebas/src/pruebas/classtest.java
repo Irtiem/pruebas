64cdce3d9b5b28eb5692ab73ae0799e6df60e902
jesus93 <bermyk93@gmail.com>
2019-07-29 19:59:38 +0200

    prueba ejercicio java

---
+++ pruebas/src/pruebas/classtest.java
@@ -0,0 +1,19 @@
+package pruebas;
+
+import javax.lang.model.element.VariableElement;
+
+public class classtest {
+
+	public static void main(String[] args) {
+		
+		
+		System.out.println("hola");
+		
+		for (int i = 0; i < 3; i++) {
+			System.out.println("hol" + i);
+		}
+		
+		
+	}
+	
+}
